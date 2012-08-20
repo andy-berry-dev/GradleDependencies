4bbd1a87d92c84558f2c18cb007c1f921e433cee
Andy Berry <andyberry88@gmail.com>
2012-08-20 19:21:41 +0100

    adding dependency method to extensions

--- src/main/groovy/com/andy_berry/gradle/plugins/Dependencies.groovy
+++ src/main/groovy/com/andy_berry/gradle/plugins/Dependencies.groovy
@@ -1,5 +1,8 @@
 package com.andy_berry.gradle.plugins
 
+import groovy.util.ConfigSlurper
+
+import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -7,12 +10,39 @@ class Dependencies implements Plugin<Project>
 {
 
 	private static final String DEFAULT_DEPENDENCIES_FILENAME = "dependencies.properties"
+	
 	private HashMap<String,String> dependencies = new HashMap<String,String>()
 	
 	void apply(Project project) 
 	{
-		project.ext.dependenciesFile = project.file(DEFAULT_DEPENDENCIES_FILENAME)
 		
+		Project dendenciesFileProject = project
+		while (dendenciesFileProject.parent != null)
+		{
+			dendenciesFileProject = dendenciesFileProject.parent
+		}
+		
+		if (!project.ext.has('dependenciesFile'))
+		{
+			project.ext.dependenciesFile = dendenciesFileProject.file(DEFAULT_DEPENDENCIES_FILENAME)
+		}
+		
+		if (project.ext.dependenciesFile.exists())
+		{
+			dependencies = new ConfigSlurper().parse( project.ext.dependenciesFile.toURL() )			
+		}
+		
+		println dependencies
+		project.ext.dependency = { String dep ->
+			if (dependencies.dep == null) {
+				throw new GradleException("No dependency found for '${dep}'.")
+			}
+			else
+			{
+				return dependencies[dep]
+			}
+
+		}
 		
 	}
 	
--- src/test/groovy/com/andy_berry/gradle/plugins/DependenciesTest.groovy
+++ src/test/groovy/com/andy_berry/gradle/plugins/DependenciesTest.groovy
@@ -14,24 +14,63 @@ class DependenciesTest {
 	public void setup()
 	{
 		project = ProjectBuilder.builder().build()
-		project.apply plugin: 'dependencies'
 	}
 	
 	@Test
-	public void testPluginCanBeAppliedUsingShortname() {
+	public void testPluginCanBeAppliedUsingShortname() 
+	{
+		project.apply plugin: 'dependencies'
 		assertTrue( project.plugins.hasPlugin(Dependencies.class) )
 	}
 	
 	@Test
-	public void testDependenciesFileExtensionIsAddedToProject() {
+	public void testDependenciesFileExtensionIsAddedToProject() 
+	{
+		project.apply plugin: 'dependencies'
 		assertTrue( project.ext.has("dependenciesFile") )
 		assertTrue( project.hasProperty("dependenciesFile") )
 	}
 	
 	@Test
-	public void testDependenciesFileCanBeOverridden() {
-		project.ext.dependenciesFile = project.file("someFile.properties")
-		assertEquals( project.file("someFile.properties"), project.ext.dependenciesFile )
+	public void testDefaultDependenciesFileIsInProjectDir() 
+	{
+		project.apply plugin: 'dependencies'
+		assertEquals( project.file("dependencies.properties"), project.ext.dependenciesFile )
 	}
 	
+	@Test
+	public void testDependenciesFileCanBeOverridden() 
+	{
+		project.ext.dependenciesFile = project.file("someFile.properties")
+		project.apply plugin: 'dependencies'
+		assertEquals( project.file("someFile.properties"), project.ext.dependenciesFile )
+	}
+
+	@Test
+	public void testDefaultDependenciesFileIsAddedToRootProject() 
+	{
+		Project childProject1 = ProjectBuilder.builder().withParent(project).build()
+		Project childProject2 = ProjectBuilder.builder().withParent(childProject1).build()
+		
+		project.apply plugin: 'dependencies'
+		childProject1.apply plugin: 'dependencies'
+		childProject2.apply plugin: 'dependencies'
+		
+		assertEquals( project.file("dependencies.properties"), project.ext.dependenciesFile )
+		assertEquals( project.file("dependencies.properties"), childProject1.ext.dependenciesFile )
+		assertEquals( project.file("dependencies.properties"), childProject2.ext.dependenciesFile )
+	}
+
+	@Test
+	public void testDependenciesDefinitionsAreLoaded()
+	{
+		File dependenciesFile = project.file("dependencies.properties")
+		dependenciesFile.createNewFile()
+		dependenciesFile.text = "someDep = 'some-group:some-id:1.2.3'"
+		project.ext.dependenciesFile = dependenciesFile
+	
+		project.apply plugin: 'dependencies'
+		assertEquals( 'some-group:some-id:1.2.3', project.dependency("someDep") )
+	}
+		
 }
